18b3b8aeb34e272feb6d1dee2574f91d7058653b
Pradhumn07 <Srivastavapradhumn@gmail.com>
2025-10-16 14:20:55 +0530

    Hyperparameter tunnung completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+#import next components from model transformation
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 
 #Configuration Class (Fixed)
 @dataclass
@@ -58,12 +63,19 @@ class DataIngestion:
             raise CustomException(e, sys)
 
 
-if __name__ == "__main__":
-    obj = DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
-    
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    
+    
+    
+    
     print("Data Ingestion Completed Successfully!")
     print("Train Data Path:", train_data)
     print("Test Data Path:", test_data)
